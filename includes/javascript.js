452b07ad43568223213df32964bfe506d58ea312
svetagimpelson <svetagimpelson@se.shenkar.ac.il>
2018-05-24 12:05:08 +0300

    layout3-not responsive

--- includes/javascript.js
+++ includes/javascript.js
@@ -1,24 +1,25 @@
-window.onload=function(){
+
+window.onload=function() {
     var i;
     for (i = 0; i < 4; i++) {
-        var newSquare=document.createElement("article");
-        var formObj=document.getElementById("jsSection");
+        var newSquare = document.createElement("article");
+        var formObj = document.getElementById("jsSection");
         formObj.appendChild(newSquare);
-        newSquare.style.height="50%";
-        newSquare.style.width="50%";
-        newSquare.style.cssFloat="left";
-        newSquare.style.opacity=Math.random();
+        newSquare.style.height = "250px";
+        newSquare.style.width = "250px";
+        newSquare.style.cssFloat = "left";
+        newSquare.style.opacity = Math.random();
     }
-}
 
-
-document.onclick=function (){
-    console.log("click")
-    var newSquare=document.createElement("article");
-    var formObj=document.getElementById("mainSection");
-    formObj.appendChild(newSquare);
-    newSquare.style.height="50%";
-    newSquare.style.width="25%";
-    newSquare.style.cssFloat="left";
-    newSquare.style.opacity=Math.random();
+    var plusElement = document.getElementById("plus")
+    plusElement.onclick = function () {
+        console.log("click")
+        var newSquare = document.createElement("article");
+        var formObj = document.getElementById("mainSection");
+        formObj.appendChild(newSquare);
+        newSquare.style.height = "250px";
+        newSquare.style.width = "250px";
+        newSquare.style.cssFloat = "left";
+        newSquare.style.opacity = Math.random();
+    }
 }
